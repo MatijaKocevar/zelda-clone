b4061fd6c828c16119afb1db6e608361378bee32
Matija Kočevar <matija.kocev@gmail.com>
2024-02-03 21:53:49 +0100

    Refactored to separate classes

--- src/components/Game/GameComponent.tsx
+++ src/components/Game/GameComponent.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Phaser from 'phaser';
-import playerSprite from '../../sprites/temp-player.png';
+import { GameScene } from './GameScene/GameScene';
 
 const GameComponent = () => {
     useEffect(() => {
@@ -16,52 +16,7 @@ const GameComponent = () => {
                     debug: true,
                 },
             },
-            scene: {
-                preload() {
-                    this.load.image('player', playerSprite);
-                },
-                create() {
-                    const gameWidth = this.sys.game.config.width as number;
-                    const platforms = this.physics.add.staticGroup();
-
-                    platforms
-                        .create(gameWidth / 2, 615, 'platform')
-                        .setScale(gameWidth / 2, 1)
-                        .refreshBody();
-
-                    const player = this.physics.add.sprite(400, 300, 'player');
-                    player.setGravityY(300);
-                    player.setBounce(0.8);
-
-                    this.physics.add.collider(player, platforms);
-
-                    const cursors = this.input.keyboard?.createCursorKeys();
-
-                    if (cursors) {
-                        this.cursors = cursors;
-                        this.player = player;
-                    }
-                    player.setCollideWorldBounds(true);
-                },
-                update() {
-                    //eslint-disable-next-line
-                    const { cursors, player } = this as Phaser.Scene;
-
-                    if (cursors) {
-                        if (cursors.left.isDown) {
-                            player.setVelocityX(-160);
-                        } else if (cursors.right.isDown) {
-                            player.setVelocityX(160);
-                        } else {
-                            player.setVelocityX(0);
-                        }
-
-                        if (cursors.up.isDown && player.body?.touching.down) {
-                            player.setVelocityY(-330);
-                        }
-                    }
-                },
-            },
+            scene: GameScene,
         };
 
         const game = new Phaser.Game(gameConfig);
