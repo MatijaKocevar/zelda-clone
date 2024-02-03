12f21b7753afb6557388ade336892821592c1a5c
Matija Kočevar <matija.kocev@gmail.com>
2024-02-03 22:51:18 +0100

    Added player spirte with movement

--- src/components/Game/GameScene/GameScene.ts
+++ src/components/Game/GameScene/GameScene.ts
@@ -1,15 +1,18 @@
-import playerSprite from '../../../sprites/temp-player.png';
+import playerSprite from '../../../sprites/player/blue/char_blue_1.png';
 import { Player } from '../../Player/Player';
 
 export class GameScene extends Phaser.Scene {
     player: Player | undefined;
 
     preload() {
-        this.load.image('player', playerSprite);
+        this.load.spritesheet('player', playerSprite, {
+            frameWidth: 56,
+            frameHeight: 56,
+        });
     }
 
     create() {
-        this.player = new Player(this);
+        this.player = new Player({ position: { x: 100, y: 500 }, scene: this });
     }
 
     update() {
