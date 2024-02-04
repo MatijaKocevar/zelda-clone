beb8952ed468b4e92837e54984b73409ab417908
Matija Kočevar <matija.kocev@gmail.com>
2024-02-04 01:55:06 +0100

    Player movement with jumping perfected

--- src/components/Game/GameComponent.tsx
+++ src/components/Game/GameComponent.tsx
@@ -2,21 +2,7 @@ import { createRef, useEffect } from 'react';
 import Phaser from 'phaser';
 import { GameScene } from './GameScene/GameScene';
 
-const preventDefaults = (event: KeyboardEvent) => {
-    if (
-        event.shiftKey &&
-        (event.key === 'd' ||
-            event.key === 'D' ||
-            event.key === 's' ||
-            event.key === 'S' ||
-            event.key === 'a' ||
-            event.key === 'A' ||
-            event.key === 'w' ||
-            event.key === 'W')
-    ) {
-        event.preventDefault();
-    }
-};
+const preventDefaults = (event: KeyboardEvent) => {};
 
 const GameComponent = () => {
     const gameComponentRef = createRef<HTMLDivElement>();
@@ -32,8 +18,8 @@ const GameComponent = () => {
     useEffect(() => {
         const gameConfig: Phaser.Types.Core.GameConfig = {
             type: Phaser.AUTO,
-            width: 800,
-            height: 600,
+            width: 600,
+            height: 400,
             parent: 'phaser-game-container',
             physics: {
                 default: 'arcade',
