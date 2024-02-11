ef13365f1123cad0f5e0eac516628aeb7384df3e
Matija Kočevar <matija.kocev@gmail.com>
2024-02-11 01:34:49 +0100

    Fix import bug

--- src/components/Game/scenes/HomeScene/HomeScene.ts
+++ src/components/Game/scenes/HomeScene/HomeScene.ts
@@ -1,26 +1,38 @@
-import playerSprite1 from '../../../assets/characters/player.png';
-import tieHome from '../../../assets/map/TieHome.png';
+import playerSprite1 from '../../../../assets/characters/player.png';
+import tieHome from '../../../../assets/map/TieHome.png';
 import { Player } from '../../entities/Player/Player';
 import { Animations } from '../../mechanics/Animations/Animations';
 
-export class HomeScene extends Phaser.Scene {
+export class HomeScene {
     player: Player | undefined;
     animations: Animations | undefined;
+    scene: Phaser.Scene;
+
+    constructor(scene: Phaser.Scene) {
+        this.scene = scene;
+    }
 
     preload() {
-        this.load.spritesheet('player1', playerSprite1, {
+        const { load } = this.scene;
+
+        load.spritesheet('player1', playerSprite1, {
             frameWidth: 144,
             frameHeight: 144,
         });
 
-        this.load.image('tie-home', tieHome);
+        load.image('tie-home', tieHome);
     }
 
     create() {
-        this.add.image(-1200, -600, 'tie-home').setOrigin(0, 0);
+        const { add } = this.scene;
 
-        this.animations = new Animations(this);
-        this.player = new Player({ position: { x: 100, y: 500 }, scene: this });
+        add.image(-1200, -600, 'tie-home').setOrigin(0, 0);
+
+        this.animations = new Animations(this.scene);
+        this.player = new Player({
+            position: { x: 100, y: 500 },
+            scene: this.scene,
+        });
     }
 
     update() {
