339e748e89c761ae2fd298ea9bdba594cd27c73e
Matija Kočevar <matija.kocev@gmail.com>
2024-02-11 21:50:06 +0100

    Add Enemy class

--- src/Game/mechanics/Animations/Animations.ts
+++ src/Game/mechanics/Animations/Animations.ts
@@ -1,15 +1,19 @@
+import { EnemyAnimations } from './components/EnemyAnimations';
 import { PlayerAnimations } from './components/PlayerAnimations';
 
 export class Animations {
     playerAnimations: PlayerAnimations;
+    enemyAnimations: EnemyAnimations;
 
     constructor(scene: Phaser.Scene) {
         this.playerAnimations = new PlayerAnimations(scene);
+        this.enemyAnimations = new EnemyAnimations(scene);
 
         this.init();
     }
 
     init() {
         this.playerAnimations.init();
+        this.enemyAnimations.init();
     }
 }
