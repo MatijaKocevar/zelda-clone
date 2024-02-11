57f26157602c6112c586740b574c1fb34d751b59
Matija Kočevar <matija.kocev@gmail.com>
2024-02-11 05:55:24 +0100

    Collisions working

--- src/Game/scenes/HomeScene/HomeScene.ts
+++ src/Game/scenes/HomeScene/HomeScene.ts
@@ -1,8 +1,10 @@
 import playerSprite1 from '../../../assets/characters/player.png';
 import tieHome from '../../../assets/map/TieHome.png';
 import tieHomeForeground from '../../../assets/map/TieHomeForeground.png';
+import collisionBlock from '../../../assets/tilesets/collision.png';
 import { Player } from '../../entities/Player/Player';
 import { Animations } from '../../mechanics/Animations/Animations';
+import { getCollisions2dArray } from './data/homeCollisions2dArray';
 
 export class HomeScene {
     player: Player | undefined;
@@ -23,6 +25,7 @@ export class HomeScene {
 
         load.image('tie-home', tieHome);
         load.image('tie-home-foreground', tieHomeForeground);
+        load.image('collision-block', collisionBlock);
     }
 
     create() {
@@ -32,7 +35,7 @@ export class HomeScene {
 
         this.animations = new Animations(this.scene);
         this.player = new Player({
-            position: { x: 2400, y: 1200 },
+            position: { x: 2400, y: 1250 },
             scene: this.scene,
         });
 
@@ -43,7 +46,36 @@ export class HomeScene {
         cameras.main.startFollow(sprite, true, 0.05, 0.05);
         cameras.main.setBounds(0, 0, 5120, 2880);
 
+        const tileSize = 16;
+        const collisions2dArray = getCollisions2dArray();
+        collisions2dArray.forEach((row, y) => {
+            row.forEach((col, x) => {
+                if (col === 1) {
+                    const block = this.scene.physics.add
+                        .staticImage(
+                            x * tileSize * 4 + 32,
+                            y * tileSize * 4 + 32,
+                            'collision-block'
+                        )
+                        .setOrigin(0, 0)
+                        .setDisplayOrigin(32, 32)
+                        .setVisible(false);
+
+                    block.body.setSize(tileSize * 4, tileSize * 4);
+                    block.setImmovable(true);
+
+                    if (this.player && this.player.sprite) {
+                        this.scene.physics.add.collider(
+                            this.player.sprite,
+                            block
+                        );
+                    }
+                }
+            });
+        });
+
         add.image(0, 0, 'tie-home-foreground').setOrigin(0, 0);
+        console.log('HomeScene created: ', getCollisions2dArray());
     }
 
     update() {
