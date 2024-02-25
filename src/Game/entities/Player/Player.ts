9c189d2cf45c7c84beb88488b50f65bd68a7af93
Matija Kočevar <matija.kocev@gmail.com>
2024-02-25 01:28:26 +0100

    Refactor: Player props simplified

--- src/Game/entities/Player/Player.ts
+++ src/Game/entities/Player/Player.ts
@@ -9,7 +9,6 @@ import { PlayerLifeBar } from './components/PlayerLifeBar';
 export class Player {
     scene: Phaser.Scene;
     position: Position;
-    input: Input;
     sprite: Phaser.Physics.Arcade.Sprite;
     playerMovement: PlayerMovement;
     playerAttack: PlayerAttack;
@@ -19,34 +18,27 @@ export class Player {
     constructor({ position, scene }: IPlayer) {
         this.scene = scene;
         this.position = position;
-        this.input = new Input(scene);
 
         this.playerStats = new PlayerStats({
             health: 2000,
             maxHealth: 2000,
             damage: 10,
         });
-        this.playerLifeBar = new PlayerLifeBar({
-            scene,
-            playerStats: this.playerStats,
-        });
+        this.playerLifeBar = new PlayerLifeBar({ player: this });
         this.sprite = scene.physics.add.sprite(
             position.x,
             position.y,
             'player1'
         );
         this.playerMovement = new PlayerMovement({
-            player: this.sprite,
-            scene,
+            player: this,
         });
         this.playerAttack = new PlayerAttack({
-            player: this.sprite,
-            scene,
-            playerMovement: this.playerMovement,
+            player: this,
         });
 
-        this.sprite.body?.setSize(25, 30);
-        this.sprite.body?.setOffset(62, 100);
+        this.sprite.body?.setSize(25, 50);
+        this.sprite.body?.setOffset(62, 70);
     }
 
     update() {
