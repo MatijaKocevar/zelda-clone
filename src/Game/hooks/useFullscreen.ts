cf29603625f86fb25141042b436b3618a5c8e69e
Matija Kočevar <matija.kocev@gmail.com>
2024-03-01 20:04:41 +0100

    Clean up code with hooks

---
+++ src/Game/hooks/useFullscreen.ts
@@ -0,0 +1,22 @@
+// useFullScreen.ts
+import { useState, useEffect } from 'react';
+
+export const useFullScreen = () => {
+    const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+
+    useEffect(() => {
+        const updateFullScreenStatus = () => setIsFullScreen(!!document.fullscreenElement);
+        document.addEventListener('fullscreenchange', updateFullScreenStatus);
+        return () => document.removeEventListener('fullscreenchange', updateFullScreenStatus);
+    }, []);
+
+    const toggleFullScreen = (): void => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen().catch(console.error);
+        } else if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    };
+
+    return { isFullScreen, toggleFullScreen };
+};
