97d4cef302316ef0447ae969129abcbdc502ac66
Matija Kočevar <matija.kocev@gmail.com>
2024-03-03 20:38:23 +0100

    Patrol movement for enemies

--- src/game/entities/Enemy/components/EnemyMovement.ts
+++ src/game/entities/Enemy/components/EnemyMovement.ts
@@ -1,21 +1,25 @@
 import { Player } from '../../Player/Player';
 import { Enemy } from '../Enemy';
-import { IEnemyMovement } from '../Enemy.types';
+import { IEnemyMovement, PatrolPath } from '../Enemy.types';
 
 export class EnemyMovement {
     private enemy: Enemy;
     private player?: Player;
     private spriteName: string;
-    private moveDirection: 'horizontal' | 'vertical';
-    private moveDistance = 300;
+    private patrolPath: PatrolPath[];
+    private patrolIndex = 0;
     private currentMoveDistance = 0;
-    private movingPositive = true;
     private detectionRange = 200;
+    private pauseTimer = 0;
+    private isPaused = false;
+    private alertSpeedMultiplier = 1.5;
+    private lastDirection: 'left' | 'right' | 'up' | 'down' | 'horizontal' =
+        'horizontal';
 
-    constructor({ enemy, moveDirection, spriteName }: IEnemyMovement) {
+    constructor({ enemy, patrolPath, spriteName }: IEnemyMovement) {
         this.enemy = enemy;
         this.spriteName = spriteName;
-        this.moveDirection = moveDirection;
+        this.patrolPath = patrolPath;
     }
 
     setPlayer(player: Player) {
@@ -23,6 +27,16 @@ export class EnemyMovement {
     }
 
     public update() {
+        if (this.isPaused) {
+            this.handlePause();
+
+            this.enemy.sprite.setVelocityX(0);
+            this.enemy.sprite.setVelocityY(0);
+
+            this.playIdleAnimation();
+            return;
+        }
+
         if (this.player) {
             const distanceToPlayer = Phaser.Math.Distance.Between(
                 this.enemy.sprite.x,
@@ -34,30 +48,34 @@ export class EnemyMovement {
             if (distanceToPlayer <= this.detectionRange) {
                 this.moveTowardsPlayer();
             } else {
-                this.handleAutomaticMovement();
+                this.followPatrolPath();
             }
         }
     }
 
     private moveTowardsPlayer() {
         if (this.player) {
-            const playerCenter = this.player.sprite.getCenter();
-            const enemyCenter = this.enemy.sprite.getCenter();
+            const playerCenterX = this.player.sprite.body?.center.x ?? 0;
+            const playerCenterY = this.player.sprite.body?.center.y ?? 0;
+            const enemyCenterX = this.enemy.sprite.body?.center.x ?? 0;
+            const enemyCenterY = this.enemy.sprite.body?.center.y ?? 0;
 
             const angle = Phaser.Math.Angle.Between(
-                enemyCenter.x ?? 0,
-                enemyCenter.y ?? 0,
-                playerCenter.x ?? 0,
-                playerCenter.y ?? 0
+                enemyCenterX,
+                enemyCenterY,
+                playerCenterX,
+                playerCenterY
             );
 
-            const velocity = 100;
-            this.enemy.sprite.setVelocityX(Math.cos(angle) * velocity);
-            this.enemy.sprite.setVelocityY(Math.sin(angle) * velocity);
+            const velocity = 100 * this.alertSpeedMultiplier;
+            const velocityX = Math.cos(angle) * velocity;
+            const velocityY = Math.sin(angle) * velocity;
+
+            this.enemy.sprite.setVelocityX(velocityX);
+            this.enemy.sprite.setVelocityY(velocityY);
 
             this.enemy.sprite.flipX =
                 this.player.sprite.x < this.enemy.sprite.x;
-
             this.enemy.sprite.anims.play(
                 `${this.spriteName}-walk-horizontal`,
                 true
@@ -65,27 +83,79 @@ export class EnemyMovement {
         }
     }
 
-    private handleAutomaticMovement() {
+    private followPatrolPath() {
+        const path = this.patrolPath[this.patrolIndex];
         const { sprite } = this.enemy;
+        const velocity = 100;
 
-        const velocity = this.movingPositive ? 100 : -100;
-        if (this.moveDirection === 'horizontal') {
-            sprite.setVelocityX(velocity);
-            sprite.anims.play(`${this.spriteName}-walk-horizontal`, true);
-            sprite.flipX = !this.movingPositive;
-        } else {
-            sprite.setVelocityY(velocity);
-            const animationName = this.movingPositive
-                ? `${this.spriteName}-walk-down`
-                : `${this.spriteName}-walk-up`;
-            sprite.anims.play(animationName, true);
+        switch (path.direction) {
+            case 'left':
+            case 'right':
+                sprite.setVelocityY(0);
+                sprite.setVelocityX(
+                    path.direction === 'right' ? velocity : -velocity
+                );
+                sprite.flipX = path.direction === 'left';
+                break;
+            case 'up':
+            case 'down':
+                sprite.setVelocityX(0);
+                sprite.setVelocityY(
+                    path.direction === 'down' ? velocity : -velocity
+                );
+                break;
         }
 
+        this.lastDirection =
+            path.direction === 'left' || path.direction === 'right'
+                ? 'horizontal'
+                : path.direction;
+
+        sprite.anims.play(
+            `${this.spriteName}-walk-${this.lastDirection}`,
+            true
+        );
+
         this.currentMoveDistance +=
-            (Math.abs(velocity) * this.enemy.scene.game.loop.delta) / 1000;
-        if (this.currentMoveDistance >= this.moveDistance) {
-            this.currentMoveDistance = 0;
-            this.movingPositive = !this.movingPositive;
+            Math.abs(velocity * this.enemy.scene.game.loop.delta) / 1000;
+        if (this.currentMoveDistance >= path.distance) {
+            this.prepareForPause();
+        }
+    }
+
+    private playIdleAnimation() {
+        let idleAnimationName = `${this.spriteName}-idle-`;
+        switch (this.lastDirection) {
+            case 'left':
+            case 'right':
+            case 'horizontal':
+                idleAnimationName += 'horizontal';
+                break;
+            case 'up':
+                idleAnimationName += 'up';
+                break;
+            case 'down':
+                idleAnimationName += 'down';
+                break;
+            default:
+                idleAnimationName += 'horizontal';
+                break;
+        }
+        this.enemy.sprite.anims.play(idleAnimationName, true);
+    }
+
+    private prepareForPause() {
+        this.currentMoveDistance = 0;
+        this.patrolIndex = (this.patrolIndex + 1) % this.patrolPath.length;
+        this.isPaused = true;
+        this.pauseTimer = 1000;
+    }
+
+    private handlePause() {
+        if (this.pauseTimer > 0) {
+            this.pauseTimer -= this.enemy.scene.game.loop.delta;
+        } else {
+            this.isPaused = false;
         }
     }
 }
