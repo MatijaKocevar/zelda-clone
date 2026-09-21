d7aa1c4d7df9b8a5f14cc9ed4c48c69a29aa2c74
Matija Kočevar <matija.kocev@gmail.com>
2026-09-22 00:34:40 +0200

    feat: add house tileset with enterable houses, room doors and collision shapes

--- src/game/areas/home/home.assets.ts
+++ src/game/areas/home/home.assets.ts
@@ -2,6 +2,7 @@ import homeMapUrl from '../../../assets/map/home/tiled/home.tmj?url';
 import tilesetWater from '../../../assets/OG/ninja-4x/TilesetWater.png';
 import tilesetField from '../../../assets/OG/ninja-4x/TilesetField.png';
 import tilesetNature from '../../../assets/OG/ninja-4x/TilesetNature.png';
+import tilesetHouse from '../../../assets/OG/ninja-4x/TilesetHouse.png';
 import { AreaDefinition, TilesetImageAsset } from '../area.types';
 import { homeEnemySpawns } from './home-enemies';
 
@@ -9,6 +10,7 @@ const homeTilesetImages: TilesetImageAsset[] = [
     { name: 'TilesetWater', key: 'home-tileset-water', path: tilesetWater },
     { name: 'TilesetField', key: 'home-tileset-field', path: tilesetField },
     { name: 'TilesetNature', key: 'home-tileset-nature', path: tilesetNature },
+    { name: 'TilesetHouse', key: 'home-tileset-house', path: tilesetHouse },
 ];
 
 export const homeArea: AreaDefinition = {
