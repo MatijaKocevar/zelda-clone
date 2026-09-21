2e378eb5c1466a8ad6d1745a0c13bb24c4961c14
Matija Kočevar <matija.kocev@gmail.com>
2026-09-21 23:48:25 +0200

    feat: restore home enemies

---
+++ src/game/areas/home/home-enemies.ts
@@ -0,0 +1,154 @@
+import { EnemySpawn } from '../area.types';
+
+export const homeEnemySpawns: EnemySpawn[] = [
+    {
+        position: { x: 2624, y: 448 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 2368, y: 896 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 1248, y: 800 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'right', distance: 100 },
+            { direction: 'down', distance: 100 },
+            { direction: 'left', distance: 100 },
+            { direction: 'up', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 3840, y: 864 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'down', distance: 100 },
+            { direction: 'left', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'right', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 608, y: 1024 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'left', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 320, y: 1408 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 1088, y: 1536 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 2048, y: 1408 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 3072, y: 1408 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 4352, y: 1408 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 4736, y: 1408 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 1504, y: 2208 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'down', distance: 100 },
+            { direction: 'left', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'right', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 3520, y: 2208 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'right', distance: 100 },
+            { direction: 'down', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'left', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 2560, y: 2176 },
+        type: 'pinkazoid',
+        patrolPath: [
+            { direction: 'left', distance: 100 },
+            { direction: 'right', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'down', distance: 100 },
+        ],
+    },
+    {
+        position: { x: 1824, y: 2560 },
+        type: 'zomboi',
+        patrolPath: [
+            { direction: 'down', distance: 100 },
+            { direction: 'left', distance: 100 },
+            { direction: 'up', distance: 100 },
+            { direction: 'right', distance: 100 },
+        ],
+    },
+];
--- src/game/dev-flags.ts
+++ src/game/dev-flags.ts
@@ -1,1 +1,1 @@
-export const ENEMIES_ENABLED = false;
+export const ENEMIES_ENABLED = true;
